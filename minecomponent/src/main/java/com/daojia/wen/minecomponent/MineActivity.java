b9cb1d77d20c4a159cf8f89f18692972ecf0a0b2
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-03-27 21:10:01 +0800

    Initial commit

---
+++ minecomponent/src/main/java/com/daojia/wen/minecomponent/MineActivity.java
@@ -0,0 +1,15 @@
+package com.daojia.wen.minecomponent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daojia.wen.mineapplication.R;
+
+public class MineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
