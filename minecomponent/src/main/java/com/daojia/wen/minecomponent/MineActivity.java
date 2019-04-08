96f446c3e9a63604bab23217dcfbb784d28cbeca
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-04-08 15:15:33 +0800

    fix the bug

--- minecomponent/src/main/java/com/daojia/wen/minecomponent/MineActivity.java
+++ minecomponent/src/main/java/com/daojia/wen/minecomponent/MineActivity.java
@@ -3,8 +3,6 @@ package com.daojia.wen.minecomponent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.daojia.wen.mineapplication.R;
-
 public class MineActivity extends AppCompatActivity {
 
     @Override
