16c191cd18daf89d113bc14ce2ef86b876aec456
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-04-14 23:25:53 +0800

    add the searchComponent

--- searchcomponent/src/main/java/com/daojia/wen/searchcomponent/searchActivity.java
+++ searchcomponent/src/main/java/com/daojia/wen/searchcomponent/searchActivity.java
@@ -1,15 +1,13 @@
-package com.daojia.wen.minecomponent;
+package com.daojia.wen.searchcomponent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.daojia.wen.mineapplication.R;
-
-public class MineActivity extends AppCompatActivity {
+public class searchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mine);
+        setContentView(R.layout.activity_search);
     }
 }
