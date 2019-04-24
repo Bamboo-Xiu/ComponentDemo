b7378163d7a8f58e7eb2fdbe544f5f86af95efa2
wenjinchao <wenjinchao01@daojia-inc.com>
2019-04-24 13:34:40 +0800

    change the componentlib's gradle file and searchService file

--- searchcomponent/src/main/java/com/daojia/wen/searchcomponent/SearchService.java
+++ searchcomponent/src/main/java/com/daojia/wen/searchcomponent/SearchService.java
@@ -1,5 +1,21 @@
 package com.daojia.wen.searchcomponent;
 
 
-public class SearchService {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.daojia.wen.componentlib.service.ISearchService;
+
+public class SearchService implements ISearchService {
+    @Override
+    public void launch(Context context, String targetClass) {
+
+    }
+
+    @Override
+    public Fragment newSearchFragment(FragmentManager fragmentManager, int viewId, Bundle bundle) {
+        return null;
+    }
 }
