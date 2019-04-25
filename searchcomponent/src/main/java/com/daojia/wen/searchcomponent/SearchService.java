3d14e8a8c32039bf08c57ca6275019cffbd80e6b
wenjinchao <wenjinchao01@daojia-inc.com>
2019-04-25 13:27:21 +0800

    add SearchFragment and finish the SearchService class

--- searchcomponent/src/main/java/com/daojia/wen/searchcomponent/SearchService.java
+++ searchcomponent/src/main/java/com/daojia/wen/searchcomponent/SearchService.java
@@ -2,6 +2,7 @@ package com.daojia.wen.searchcomponent;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,11 +12,16 @@ import com.daojia.wen.componentlib.service.ISearchService;
 public class SearchService implements ISearchService {
     @Override
     public void launch(Context context, String targetClass) {
-
+        Intent intent = new Intent(context, searchActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 
     @Override
     public Fragment newSearchFragment(FragmentManager fragmentManager, int viewId, Bundle bundle) {
-        return null;
+        SearchFragment searchFragment = new SearchFragment();
+        searchFragment.setArguments(bundle);
+        fragmentManager.beginTransaction().add(viewId,searchFragment).commit();
+        return searchFragment;
     }
 }
